cdee710914121adcef5b60eb60161044d28857d1
Daniel Weiß <wanieldeiss@gmail.com>
2021-12-08 23:43:43 +0100

    add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
