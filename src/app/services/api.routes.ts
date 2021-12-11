d16467a26df47aef1a7605ea2cf9a852e8d3c327
Daniel Weiß <wanieldeiss@gmail.com>
2021-12-11 12:08:26 +0100

    add get pokemon

--- src/app/services/api.routes.ts
+++ src/app/services/api.routes.ts
@@ -1,5 +1,6 @@
 const API_BASE_URL = 'https://pokeapi.co/api/v2'
 
 export default {
-    getAllPokemon: (limit?: number) => `${API_BASE_URL}/pokemon/${limit ? '?limit='+limit : ''}`
+    getAllPokemon: (limit?: number) => `${API_BASE_URL}/pokemon/${limit ? '?limit='+limit : ''}`,
+    getPokemon: (name: string) => `${API_BASE_URL}/pokemon/${name}`
 };
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { pluck } from 'rxjs';
+import { Observable, pluck } from 'rxjs';
+import { Pokemon } from '../interfaces/pokemon';
 import API_ROUTES from './api.routes';
 
 @Injectable({
@@ -12,5 +13,9 @@ export class PokemonService {
 
   getAllPokemon(limit?: number) {
     return this.httpService.get(API_ROUTES.getAllPokemon(limit)).pipe(pluck('results'));
+  }
+
+  getPokemon(name: string) {
+    return this.httpService.get<Pokemon>(API_ROUTES.getPokemon(name))
   }
 }
