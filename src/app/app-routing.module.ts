ffc4098238c5ce8fbfc833ffe5bfc651ed5472b3
Daniel Weiß <wanieldeiss@gmail.com>
2021-12-11 12:07:42 +0100

    add details routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './pages/details/details.component';
 import { IndexComponent } from './pages/index/index.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'index', component: IndexComponent },
+  { path: 'pokemon/:pokemonName', component: DetailsComponent},
   { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
